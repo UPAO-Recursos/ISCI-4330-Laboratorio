345575d75877c99c3817585f3cd33f2296d3625d
Walter Leturia <leturia@live.com>
2024-04-30 16:43:12 +0000

    load dynamic data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import { useNavigate } from "react-router-dom";
+import Gatos from './data/gatos.json'
 
 const feather = require('feather-icons');
 
@@ -7,26 +8,7 @@ setTimeout(() => {
     feather.replace();
 }, 1000);
 
-const gatos = [
-    {
-        name: "Gato 1",
-        slug: 'gato-1',
-        description: "Descripcion Gato 1",
-        picture: 'https://s1.eestatic.com/2020/05/18/como/gatos-mascotas-trucos_490961518_152142875_1706x960.jpg'
-    },
-    {
-        name: "Gato 2",
-        slug: 'gato-2',
-        description: "Descripcion Gato 2",
-        picture: 'https://static.nationalgeographic.es/files/styles/image_3200/public/nationalgeographic_1468962.jpg?w=1600&h=900'
-    },
-    {
-        name: "Gato 3",
-        slug: 'gato-3',
-        description: "Descripcion Gato 3",
-        picture: 'https://fotografias.lasexta.com/clipping/cmsimages02/2019/01/25/DB41B993-B4C4-4E95-8B01-C445B8544E8E/69.jpg?crop=4156,2338,x0,y219&width=1280&height=720&optimize=high&format=webply'
-    },
-];
+
 
 const Main = () => {
     const navigate = useNavigate();
@@ -38,7 +20,7 @@ const Main = () => {
     return <main>
         <Carousel>
             {
-                gatos.map(it => {
+                Gatos.map(it => {
                     return (<Carousel.Item onClick={(e) => {
                         e.preventDefault(); handleClick(it.slug)
                     }}>
--- src/single.js
+++ src/single.js
@@ -1,6 +1,15 @@
+import Gatos from './data/gatos.json'
+
 const Single = () => {
+
+    const currentRoute = window.location.pathname;
+    const gato = Gatos.find(it => currentRoute.includes(it.slug))
+
     return (<>
-        <h1>Pagina simple</h1>
+        <h1>{gato.name}</h1>
+        <hr></hr>
+        <img src={gato.picture}></img>
+        <p>{gato.description}</p>
     </>);
 }
 
