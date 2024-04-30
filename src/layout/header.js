ce55056800eab399b6b1ff405d690a33b115126c
Walter Leturia <leturia@live.com>
2024-04-30 14:37:23 +0000

    contenido

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,18 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Mis Gatos</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                    </ul>
-                </div>
+                <Link class="navbar-brand" to="/">Mis gatos</Link>
             </div>
         </nav>
     </header>;
